36ba3c4a473d8531e8b71a3d84f1d74492c9d879
Thiago Esteves <calori@gmail.com>
2025-06-02 21:13:49 -0300

    Enabling tls

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ectoapp.MixProject do
   def project do
     [
       app: :ectoapp,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
